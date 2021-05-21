494597d39517f65f6a77088a80c1f48f34d9ee3a
seikhou <seikhoudanfakha@gmail.com>
2021-05-21 10:07:10 +0000

    third commit

---
+++ SanelaProgram/src/test/java/com/saraya/SanelaProgram/SanelaProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saraya.SanelaProgram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanelaProgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
